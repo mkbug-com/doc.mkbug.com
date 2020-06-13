789ec71a1a3275088dad367cd3b4b2eb3246bd6c
梁亚辉 <liangyahui@didiglobal.com>
2020-06-13 13:00:48 +0800

    update

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,33 +8,62 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
+// List of projects/orgs using your project for the users page.
+const users = [
+  {
+    caption: 'User1',
+    // You will need to prepend the image path with your baseUrl
+    // if it is not '/', like: '/test-site/img/image.jpg'.
+    image: '/img/undraw_open_source.svg',
+    infoLink: 'https://www.facebook.com',
+    pinned: true,
+  },
+];
+
 const siteConfig = {
   title: 'Mkbug.js', // Title for your website.
   tagline: 'A OOP style declare Nodejs framework base on Express.js！',
-  desc: '一个基于Express.js封装的OOP风格声明式Nodejs Restful api框架。',
   url: 'https://doc.mkbug.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
+  // For github.io type URLs, you would set the url and baseUrl like:
+  //   url: 'https://facebook.github.io',
+  //   baseUrl: '/test-site/',
 
+  // Used for publishing and more
+  projectName: 'Mkbug.com',
+  organizationName: 'ryouaki',
+  // For top-level user or org sites, the organization is still the same.
+  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+  //   organizationName: 'JoelMarcey'
+
+  // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'quick-start', label: 'Docs'},
     {doc: 'base-controller', label: 'API'},
     {blog: true, label: 'Change Log'}
   ],
 
+  // If you have users set above, you add it here:
+  users,
+
   /* path to images for header/footer */
   headerIcon: 'img/favicon.ico',
+  footerIcon: 'img/favicon.ico',
   favicon: 'img/favicon.ico',
 
   /* Colors for website */
   colors: {
-    primaryColor: '#6e6e6e',
-    secondaryColor: '#e6e6e6',
+    primaryColor: '#136069',
+    secondaryColor: '#0d4349',
   },
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
     theme: 'default',
   },
+
+  // Add custom scripts here that would be placed in <script> tags.
+  scripts: [],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
