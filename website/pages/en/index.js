76c6be537f25648e76d4f335254e5751a059c91d
梁亚辉 <liangyahui@didiglobal.com>
2020-06-26 15:59:15 +0800

    update

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -7,6 +7,14 @@
 
 const React = require('react');
 
+var _hmt = _hmt || [];
+(function() {
+var hm = document.createElement("script");
+hm.src = "https://hm.baidu.com/hm.js?74c3c27b27c030d36561f0d6cb99759b";
+var s = document.getElementsByTagName("script")[0]; 
+s.parentNode.insertBefore(hm, s);
+})();
+
 class Index extends React.Component {
   render() {
     const { config, language = '' } = this.props;
